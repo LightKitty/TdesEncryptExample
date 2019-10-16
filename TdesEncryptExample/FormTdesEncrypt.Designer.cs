f70305a4124e3f5df1230e2715c8c1a5700dc631
wnmwi <383929371@qq.com>
2019-10-16 20:35:59 +0800

    第一次提交

---
+++ TdesEncryptExample/FormTdesEncrypt.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace TdesEncryptExample
+{
+    partial class FormTdesEncrypt
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypt = new System.Windows.Forms.TextBox();
+            this.textBoxDecrypt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "原始字符串：";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(110, 12);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(462, 23);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(110, 41);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(100, 25);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "开始加密解密";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "加密私钥：";
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(110, 72);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(462, 23);
+            this.textBoxKey.TabIndex = 4;
+            // 
+            // textBoxEncrypt
+            // 
+            this.textBoxEncrypt.Location = new System.Drawing.Point(110, 101);
+            this.textBoxEncrypt.Name = "textBoxEncrypt";
+            this.textBoxEncrypt.Size = new System.Drawing.Size(462, 23);
+            this.textBoxEncrypt.TabIndex = 5;
+            // 
+            // textBoxDecrypt
+            // 
+            this.textBoxDecrypt.Location = new System.Drawing.Point(110, 130);
+            this.textBoxDecrypt.Name = "textBoxDecrypt";
+            this.textBoxDecrypt.Size = new System.Drawing.Size(462, 23);
+            this.textBoxDecrypt.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "加密后字符串：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "解密后字符串：";
+            // 
+            // FormTdesEncrypt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 172);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxDecrypt);
+            this.Controls.Add(this.textBoxEncrypt);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormTdesEncrypt";
+            this.Text = "tdes对称加密";
+            this.Load += new System.EventHandler(this.FormTdesEncrypt_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.TextBox textBoxEncrypt;
+        private System.Windows.Forms.TextBox textBoxDecrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TdesEncryptExample/FormTdesEncrypt.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TdesEncryptExample
+{
+    public partial class FormTdesEncrypt : Form
+    {
+        public FormTdesEncrypt()
+        {
+            InitializeComponent();
+        }
+
+        private void FormTdesEncrypt_Load(object sender, EventArgs e)
+        {
+            textBoxEncrypt.ReadOnly = true;
+            textBoxDecrypt.ReadOnly = true;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            string str = textBoxInput.Text;
+            if (str.Length == 0)
+            {
+                MessageBox.Show("请输入被加密的字符串");
+                return;
+            }
+            //加密
+            try
+            {
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                //随机生成密钥Key 和初始化向量IV
+                tdes.GenerateKey();
+                tdes.GenerateIV();
+                textBoxKey.Text = Encoding.UTF8.GetString(tdes.Key);
+                //得到加密后的字节流
+                byte[] encryptedBytes = EncryptText(str, tdes.Key, tdes.IV);
+                //显示加密后的字符串
+                textBoxEncrypt.Text = Encoding.UTF8.GetString(encryptedBytes);
+                //解密
+                string decryptString = DecryptText(encryptedBytes, tdes.Key, tdes.IV);
+                //显示解密后的字符串
+                textBoxDecrypt.Text = decryptString;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "出错");
+            }
+        }
+
+        private byte[] EncryptText(string str, byte[] Key, byte[] IV)
+        {
+            //创建一个内存流
+            MemoryStream memoryStream = new MemoryStream();
+            //使用传递的私钥和IV 创建加密流
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+            new TripleDESCryptoServiceProvider().CreateEncryptor(Key, IV),
+            CryptoStreamMode.Write);
+            //将传递的字符串转换为字节数组
+            byte[] toEncrypt = Encoding.UTF8.GetBytes(str);
+            try
+            {
+                //将字节数组写入加密流,并清除缓冲区
+                cryptoStream.Write(toEncrypt, 0, toEncrypt.Length);
+                cryptoStream.FlushFinalBlock();
+                //得到加密后的字节数组
+                byte[] encryptedBytes = memoryStream.ToArray();
+                return encryptedBytes;
+            }
+            catch (CryptographicException err)
+            {
+                throw new Exception("加密出错：" + err.Message);
+            }
+            finally
+            {
+                cryptoStream.Close();
+                memoryStream.Close();
+            }
+        }
+
+        private string DecryptText(byte[] dataBytes, byte[] Key, byte[] IV)
+        {
+            //根据加密后的字节数组创建一个内存流
+            MemoryStream memoryStream = new MemoryStream(dataBytes);
+            //使用传递的私钥、IV 和内存流创建解密流
+            CryptoStream cryptoStream = new CryptoStream(memoryStream,
+            new TripleDESCryptoServiceProvider().CreateDecryptor(Key, IV),
+            CryptoStreamMode.Read);
+            //创建一个字节数组保存解密后的数据
+            byte[] decryptBytes = new byte[dataBytes.Length];
+            try
+            {
+                //从解密流中将解密后的数据读到字节数组中
+                cryptoStream.Read(decryptBytes, 0, decryptBytes.Length);
+                //得到解密后的字符串
+                string decryptedString = Encoding.UTF8.GetString(decryptBytes);
+                return decryptedString;
+            }
+            catch (CryptographicException err)
+            {
+                throw new Exception("解密出错：" + err.Message);
+            }
+            finally
+            {
+                cryptoStream.Close();
+                memoryStream.Close();
+            }
+        }
+    }
+}
+
